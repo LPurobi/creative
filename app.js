7790cb197173869ed5ce2f764764c59697a56327
Purobi Rahman <purobi@Irfans-iMac.local>
2017-04-19 01:10:00 +0600

    navbar color chng

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ $(document).scroll(function(){
     if($(this).scrollTop() > a)
     {   
        $('.navbar-default').css({"background":"white"});
+       $('.navbar-default .navbar-nav>li>a').css({"color:black"})
     } else {
        $('.navbar-default').css({"background":"transparent"});
     }
